976178290029be8423945dc4fb6334bca4245893
Cameron Gagnon <cgagnon@umich.edu>
2015-09-12 09:32:08 -0400

    removed extraneous comments

--- scripts/main.js
+++ scripts/main.js
@@ -16,21 +16,3 @@ function onClickHandler(info) {
 }
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
-
-/*"use strict"
-chrome.runtime.onInstalled.addListener(function (){
-    var context = "selection";
-    var title = "Mathemorphosize";
-    var id = chrome.contextMenus.create({"title": title,
-                                         "contexts": [context],
-                                         "id": "context" + context});
-});
-
-chrome.contextMenus.onClicked.addListener(onClickHandler);
-
-function onClickHandler(info, tab){
-    var sText = info.selectionText;
-    var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
-    window.open(url, '_blank');
-    console.log("Opening url " + url);
-}*/
