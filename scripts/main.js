d9181d687b4002c14ea689c678ce42c417a966d5
Antonio Mika <antoniomika@gmail.com>
2015-09-12 17:51:52 -0400

    Fixed merge issues

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,7 @@ chrome.runtime.onInstalled.addListener(function () {
 
 
 function onClickHandler(e) {
-<<<<<<< HEAD
-    console.log(e);
-=======
     var pop = popup("../popup.html");
->>>>>>> 83a2913af3593fb7a4b66f560a29015a8901ec58
     var sel_text = e.selectionText;
     var jax;
     // Does not access the document so MathJax is not doing anything
@@ -20,7 +16,6 @@ function onClickHandler(e) {
             function () {jax = MathJax.Hub.getAllJax("math")[0]
     });
 
-<<<<<<< HEAD
     MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
 //,
 //            function () {jax = MathJax.Hub.getAllJax("preview")[0]
@@ -40,15 +35,6 @@ function popup(url, e) {
             }
         }
     });
-=======
-    MathJax.Hub.Queue(["Text", jax, sel_text]);
-    popup("../popup.html");
 }
-
-function popup(url) {
-    return window.open(url, "window", "width=300,height=260,top=150, left=300,status=yes");
->>>>>>> 83a2913af3593fb7a4b66f560a29015a8901ec58
-}
-
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
