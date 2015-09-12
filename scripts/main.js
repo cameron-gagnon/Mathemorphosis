806ac131fbe54fb64982806e4d0d7982ad222086
Antonio Mika <antoniomika@gmail.com>
2015-09-12 18:58:32 -0400

    Fixes

--- scripts/main.js
+++ scripts/main.js
@@ -17,9 +17,14 @@ function onClickHandler(e) {
 function popup(url, e) {
     chrome.windows.create({'url': url, 'type': 'panel', 'height': 260, 'width': 300, 'top': 150, 'left': 300}, function(newWindow) {
         var windows = chrome.extension.getViews();
+        var found = false;
         for(var i in windows) {
-            if(windows[i].location.pathname == "/popup.html") {
+            if(windows[i].location.pathname == "/popup.html" && !found) {
+                found = true;
                 windows[i].document.getElementById("highlighted").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&chl=' + encodeURIComponent(e.selectionText) + '"/>';
+                windows[i].focus();
+            } else {
+                windows[i].close();
             }
         }
     });
