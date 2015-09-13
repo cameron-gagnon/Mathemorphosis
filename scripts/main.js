4eb472db31f87401df209ee77162b64869b0a2fe
Cameron Gagnon <cgagnon@umich.edu>
2015-09-13 00:31:50 -0400

    trying to figure out the darn mouse coordinates

--- scripts/main.js
+++ scripts/main.js
@@ -7,26 +7,45 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 
-function onClickHandler(e) {
-    var sel_text = e.selectionText;
-    var jax;
-    // Does not access the document so MathJax is not doing anything
-    var pop = popup("../popup.html", e);
+function requestMouseLoc() {
+  chrome.runtime.sendMessage({greeting: "mouse_loc"}, function(response){
+    return response; 
+  }); 
 }
 
-function popup(url, e) {
-    chrome.windows.create({'url': url, 'type': 'panel', 'height': 260, 'width': 300, 'top': 150, 'left': 300}, function(newWindow) {
-        var windows = chrome.extension.getViews();
-        var found = false;
-        windows.forEach(function(data, elem) {
-            if(data.location.pathname == "/popup.html" && !found) {
-                found = true;
-                data.document.getElementById("highlighted").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&chl=' + encodeURIComponent(e.selectionText) + '"/>';
-                data.focus();
-            } else {
-                data.close();
-            }
-        });
+function get_mouse() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {greeting: "mouse_loc"}, function(response) {
+        console.log(response); 
+        return response;
+      });
+    });
+};
+
+function onClickHandler(e) {
+    mouse_data = get_mouse();
+    var sel_text = e.selectionText;
+    var pop = popup("../popup.html", e, mouse_data);
+}
+
+
+function popup(url, e, mouse_data) {
+    console.log(e);
+    console.log(mouse_data);
+    chrome.windows.create({'url': url, 'type': 'panel', 'height': 100, 'width': 300, 'top': 550, 'left': 300}, function(newWindow) {
+        setTimeout(function () { 
+            var windows = chrome.extension.getViews();
+            var found = false;
+            windows.forEach(function(data, elem) {
+                if (data.location.pathname == "/popup.html" && !found) {
+                    found = true;
+                    data.document.getElementById("highlighted").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(e.selectionText) + '"/>';
+                    data.focus();
+                } else {
+                    data.close();
+                }
+            });
+        }, 25);
     });
 }
 
