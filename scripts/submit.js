616beac6b58cb9888bc83a5ac623baaa81c5efad
Cameron Gagnon <cgagnon@umich.edu>
2015-09-13 11:54:52 -0400

    remove submit button after submit and check for empty field so we don't do anything

--- scripts/submit.js
+++ scripts/submit.js
@@ -3,5 +3,7 @@ setTimeout(function() {
     button.addEventListener("click", function() {
         document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
         document.getElementById("prompt").innerHTML = "Here is your expression:";
+        button.remove();
     }, false);
+         
 }, 500);
