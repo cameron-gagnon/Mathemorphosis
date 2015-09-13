012401e5c56347ee3f95c3245a3a8bd50f4555d4
Cameron Gagnon <cgagnon@umich.edu>
2015-09-13 12:30:19 -0400

    push push push

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     var button = document.getElementById("submit");
     if (document.getElementById("userinput").value == '')
-		break;
+		return;
     button.addEventListener("click", function() {
         document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
         document.getElementById("prompt").innerHTML = "Here is your expression:";
