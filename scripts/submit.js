602e1b4790677f4f126ae0ba1c0743465c025a40
jchamer <jchamer@umich.edu>
2015-09-13 11:56:09 -0400

    added trying to refocus on button click

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,7 +1,9 @@
 setTimeout(function() {
     var button = document.getElementById("submit");
-    if (document.getElementById("userinput").value == '')
+    if (document.getElementById("userinput").value == '') {
+		//document.getElementById("userinput").focus();
 		break;
+	}
     button.addEventListener("click", function() {
         document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
         document.getElementById("prompt").innerHTML = "Here is your expression:";
