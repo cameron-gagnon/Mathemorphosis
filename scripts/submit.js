d847f54cc65b02f55a540ef2d00d1ed53d5cfdb8
Antonio Mika <antoniomika@gmail.com>
2015-09-13 11:05:29 -0400

    Fixed the stuff for the browser action

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,13 +1,7 @@
-window.onload = function() {
-    setTimeout( function(){
-        document.getElementById('submit').onclick = function() {
-            var vals_to_format = document.getElementById('userinput').value;
-            chrome.tabs.create({url: '../popup.html?=' + vals_to_format}, function (callback) {
-               console.log(callback);
-            });
-        }
-    }, 1000);
-}
-        
-data.document.getElementById("highlighted").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(e.selectionText) + '"/>';
- 
+setTimeout(function() {
+    var button = document.getElementById("submit");
+    button.addEventListener("click", function() {
+        document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
+        document.getElementById("prompt").innerHTML = "Here is your expression:";
+    }, false);
+}, 500);
