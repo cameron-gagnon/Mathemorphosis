86d03ea6ed73721258478e032e4d1f3b8f1eca1e
jchamer <jchamer@umich.edu>
2015-09-13 23:29:01 -0400

    fixed the submit your own box

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,8 +1,7 @@
 setTimeout(function() {
     var button = document.getElementById("submit");
     if (document.getElementById("userinput").value == '') {
-		//document.getElementById("userinput").focus();
-		break;
+		return;
 	}
     button.addEventListener("click", function() {
         document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
