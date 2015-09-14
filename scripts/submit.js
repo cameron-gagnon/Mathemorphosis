0480dd481ad68db0cc0f4f07258dd97b9582bbda
Cameron Gagnon <cgagnon@umich.edu>
2015-09-14 13:17:41 -0400

    reformatted code

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,18 +1,14 @@
 setTimeout(function() {
     var button = document.getElementById("submit");
-<<<<<<< HEAD
-    if (document.getElementById("userinput").value == '')
-		return;
-=======
-    if (document.getElementById("userinput").value == '') {
-		//document.getElementById("userinput").focus();
-		break;
-	}
->>>>>>> 097d741af0510f6e40fe0dad0480ac9365d933b6
-    button.addEventListener("click", function() {
-        document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&<290>x<height>&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
-        document.getElementById("prompt").innerHTML = "Here is your expression:";
-        button.remove();
-    }, false);
-         
+    button.addEventListener("click", function() {sendRequest(button)}, false);
 }, 500);
+
+function sendRequest(e, button) {
+    if (document.getElementById("userinput").value == ''){
+        alert("Nothing entered"); 
+    } else {
+        document.getElementById("area").innerHTML = '<img src="https://chart.googleapis.com/chart?cht=tx&chl=' + encodeURIComponent(document.getElementById("userinput").value) + '"/>';
+        document.getElementById("prompt").innerHTML = "Here is your expression:";
+        consolve.log(button);
+    }
+}
